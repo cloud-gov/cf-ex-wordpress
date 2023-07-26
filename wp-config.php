a7f13f35455c9a16c36b756daf21fb94e79cd241
Mark Boyd <mark.boyd@gsa.gov>
2023-07-26 14:53:51 -0400

    update link in provided config

--- wp-config.php
+++ wp-config.php
@@ -4,8 +4,7 @@
  *
  * This file has the following configurations: MySQL settings, Table Prefix,
  * Secret Keys, WordPress Language, and ABSPATH. You can find more information
- * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. You can get the MySQL settings from your web host.
+ * by visiting {@link https://developer.wordpress.org/apis/wp-config-php/}. You can get the MySQL settings from your web host.
  *
  * This file is used by the wp-config.php creation script during the
  * installation. You don't have to use the web site, you can just copy this file
