646183ec206e346f534a2014ec8d8d149298c858
Mark Boyd <mark.boyd@gsa.gov>
2023-08-01 16:58:50 -0400

    update bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-# Copy wordpress files into web root folder (htdocs)
-cd "$HOME/app/wordpress" || exit
-cp -R ./* "$HOME/app/htdocs"
+APP_ROOT="$HOME"
+
+# $HOME env var changes depending on whether you're in the
+# running "lifecycle" shell or SSH session shell
+if [[ $HOME != *"app"* ]]; then
+  APP_ROOT="$APP_ROOT/app"
+fi
+
+# Copy wordpress files into web root folder if they
+# haven't been already
+if [ ! -d "$APP_ROOT/htdocs/wp-content" ]; then
+  echo "Copying Wordpress files into place"
+  cd "$APP_ROOT/wordpress" || exit
+  cp -R ./* "$APP_ROOT/htdocs"
+fi
